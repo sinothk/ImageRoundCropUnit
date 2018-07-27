fc1c56cbd83bbe1e4bc4f2934b6524fc450adcb8
LiangYutao <sinothk@126.com>
2018-07-27 16:20:52 +0800

    处理传值问题！

--- app/src/main/java/com/sinothk/image/crop/round/demo/ImageRoundCropDemoActivity.java
+++ app/src/main/java/com/sinothk/image/crop/round/demo/ImageRoundCropDemoActivity.java
@@ -11,7 +11,6 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import com.sinothk.image.crop.round.FileUtil;
 import com.sinothk.image.crop.round.ImageRoundCropActivity;
 
 import java.io.File;
